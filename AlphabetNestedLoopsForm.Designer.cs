0b5eba660e208e7e95c764f871b378204bb5da53
Tolulope24 <36515830+Tolulope24@users.noreply.github.com>
2018-04-09 13:10:25 -0400

    Completed

--- AlphabetNestedLoopsForm.Designer.cs
+++ AlphabetNestedLoopsForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AlphabetNestedLoopsToluA
 {
-    partial class Form1
+    partial class frmAlphabetNestedLoops
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstAlphabetNestedLoops = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(92, 65);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstAlphabetNestedLoops
+            // 
+            this.lstAlphabetNestedLoops.FormattingEnabled = true;
+            this.lstAlphabetNestedLoops.Location = new System.Drawing.Point(61, 137);
+            this.lstAlphabetNestedLoops.Name = "lstAlphabetNestedLoops";
+            this.lstAlphabetNestedLoops.Size = new System.Drawing.Size(143, 238);
+            this.lstAlphabetNestedLoops.TabIndex = 1;
+            // 
+            // frmAlphabetNestedLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(264, 387);
+            this.Controls.Add(this.lstAlphabetNestedLoops);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmAlphabetNestedLoops";
+            this.Text = "Alphabet Nested Loops";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstAlphabetNestedLoops;
     }
 }
 
--- AlphabetNestedLoopsForm.cs
+++ AlphabetNestedLoopsForm.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ * Created by: Tolulope Adebayo
+ * Created on: 09-04-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Numbers Nested Loops
+ * This program...
+*/
 namespace AlphabetNestedLoopsToluA
 {
-    public partial class Form1 : Form
+    public partial class frmAlphabetNestedLoops : Form
     {
-        public Form1()
+        public frmAlphabetNestedLoops()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // identify the Counters
+            int FirstCounter;
+            int SecondCounter;
+            String NestedCodeSecondCharacter;
+            String NestedCodeFirstCharacter;
+            // Clear Objects From the list box
+            this.lstAlphabetNestedLoops.Items.Clear();
+            // Loop through numbers from 97 to 123
+            for (FirstCounter = 65; FirstCounter <= 90; FirstCounter++)
+            {
+                NestedCodeFirstCharacter = char.ConvertFromUtf32(FirstCounter);
+                // For each of the numbers above, loop through again writing beside it the second numbers from 97 to 123
+                for ( SecondCounter = 97; SecondCounter <= 122; SecondCounter++)
+                {
+                    NestedCodeSecondCharacter = char.ConvertFromUtf32(SecondCounter);
+                    this.lstAlphabetNestedLoops.Items.Add(NestedCodeFirstCharacter + "-> " + NestedCodeSecondCharacter);
+                }
+            }
+
+
         }
     }
 }
